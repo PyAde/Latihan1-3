75c8eca0ac38c3dc54fcdfe108e13393fb16b8a1
PyAde <108620649+PyAde@users.noreply.github.com>
2023-02-13 13:20:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+## Latihan 1
+print("2 + 3") # hasil output ketika dijalankan di terminal menghasilkan hasil yaitu  2 + 3
+print(2 + 3) # hasil output ketika dijalankan di terminal menghasilkan hasil yaitu 5
+print("2" + "3") # hasil output ketika dijalankan di terminal menghasilkan hasil yaitu 23
+# makna tanda petik pada python adalah untuk menandakan sebuah string, print("2 + 3"),
+# merupakan sebuah string yang dimana ketika dijalan di sebuah terminal
+# akan menghasilkan output 2 + 3, print (2 + 3) merupakan tipe data int yang dimana menghasilkan
+# hasil berupa tipe data int yaitu 5, print("2" + "3") merupakan sebuah program yang menambahkan 
+# sebuah nilai string yaitu "2" dan "3" dan menghasilkan hasil output 23
+
+## Latihan 2
+bil1 = 10
+bil2 = 5
+jumlah = bil1 + bil2
+jumlah
